75fd34c707536823cf957b2776c4e6a9a627f467
DouglaSantos777 <fdsantosdec@aluno.uespi.br>
2025-01-13 03:58:34 -0300

    feat: add models of characters

--- chars/models.py
+++ chars/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+class Char(models.Model):
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    name = models.CharField(max_lenght=100, null=False, blank=False)
+    age = models.PositiveIntegerField(blank=True, null=True)
+    genre = models.CharField(max_length=20, choices=[('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other')], blank=True, null=True)
+    affiliation = models.CharField(max_length=100, blank=True, null=True)
+    status = models.CharField(max_length=50, choices=[('Alive'), ('Dead'), ('Unknown')], blank=True, null=True)
+
+def __str__(self):
+        return self.name
